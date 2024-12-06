e3769bde263a297b44bd53a07c0dd481056a19c0
Karthika K <119489367+karthikajayasakthi@users.noreply.github.com>
2024-12-06 08:21:22 +0530

    Update D4Rotatearray.py

--- D4Rotatearray.py
+++ D4Rotatearray.py
@@ -1,1 +1,14 @@
 
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        n=len(arr)
+        d%=n
+        reverse(arr,0,d-1)
+        reverse(arr,d,n-1)
+        reverse(arr,0,n-1)
+def reverse(arr,start,end):
+    while start<end:
+        arr[start],arr[end]=arr[end],arr[start]
+        start+=1
+        end-=1
