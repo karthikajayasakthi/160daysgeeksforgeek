d136dbafc74a6c6c6b2c7bb007afad04d6d3cf17
Karthika K <119489367+karthikajayasakthi@users.noreply.github.com>
2024-12-06 08:15:04 +0530

    Update D2Moveallzerostoend.py

--- D2Moveallzerostoend.py
+++ D2Moveallzerostoend.py
@@ -1,1 +1,11 @@
-
+class solution:
+  def pushZerosToEnd(self,arr):
+    count=0
+    leng=len(arr)
+    for j in range(leng):
+      if arr[j]!=9:
+        arr[count]=arr[j]
+        count+=1
+    while count<leng:
+      arr[count]=0
+      count+=1
